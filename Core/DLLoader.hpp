8e4a5623b6b00eae460d3e6ffea7b98838b9ac57
PerAlban <alban.peralta@epitech.eu>
2024-03-21 14:14:37 +0100

    :zap: Delete useless raw pointer and create a new std::execption

--- Core/DLLoader.hpp
+++ Core/DLLoader.hpp
@@ -18,31 +18,43 @@ class DLLoader {
 public :
     class Exception : public std::exception {
     public :
-        Exception() : _msg(dlerror()) {};
+        Exception(std::string const &msg) : _msg(msg) {};
         [[nodiscard]] const char *what() const noexcept override {
-                return _msg.c_str();
+            return _msg.c_str();
         }
     private :
         std::string _msg;
     };
 
-    DLLoader(const std::string &path) {
+    class dlException : public Exception {
+    public :
+        dlException() : Exception(dlerror()) {};
+    };
+
+    class TypeException : public Exception {
+    public :
+        TypeException() : Exception("Invalid type") {};
+    };
+
+    DLLoader(const std::string path) {
         lib = std::unique_ptr<void, decltype(&dlclose)>(dlopen(path.c_str(), RTLD_LAZY), dlclose);
         if (!lib)
-            throw Exception();
+            throw dlException();
     };
     
     ~DLLoader() {
         if (lib)
-            dlclose(lib);
+            dlclose(lib.get());
     };
 
-    std::unique_ptr<T> getInstance() {
-        function<std::unique_ptr<T>()> sym;
+    std::unique_ptr<T> getInstance(const std::string type) {
+        std::function<std::unique_ptr<T>()> sym;
 
-        sym = (std::unique_ptr<T>(*)())(dlsym(lib, "loadInstance"));
+        if (type != "loadGameInstance" && type != "loadGraphicInstance")
+            throw TypeException();
+        sym = (std::unique_ptr<T>(*)())(dlsym(lib.get(), type.c_str()));
         if (!sym)
-            throw Exception();
+            throw dlException();
         return sym();
     }
 private:
