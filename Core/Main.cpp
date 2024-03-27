daaab5c10894f585328db4dbaf22132e8a5877e6
PerAlban <alban.peralta@epitech.eu>
2024-03-27 16:59:26 +0100

    Auto stash before merge of "3-create-the-main-loop-of-the-arcade" and "origin/main"

--- Core/Main.cpp
+++ Core/Main.cpp
@@ -5,7 +5,24 @@
 ** Main.cpp
 */
 
-int main(void)
+#include <iostream>
+#include "DLLoader.hpp"
+#include "Core.hpp"
+
+static std::string CheckArgs(int ac, char **av)
 {
+    if (ac != 2) {
+        std::cerr << "Usage: " << av[0] << " [path_to_graphical_library.so]" << std::endl;
+        exit(84);
+    }
+    return av[1];
+}
+
+int main(int ac, char **av)
+{
+    std::string pathToGraphLib = CheckArgs(ac, av);
+    Arcade::Core core(pathToGraphLib);
+    std::cout << "Core created" << std::endl;
+    core.execArcade();
     return 0;
 }
