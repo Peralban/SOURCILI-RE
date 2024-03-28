36b6c761cd721b2a29a361be6883296f61552253
PerAlban <alban.peralta@epitech.eu>
2024-03-28 15:13:58 +0100

    :twisted_rightwards_arrows: Pull Hpp files from shared repo

--- Games/IGame.hpp
+++ Games/IGame.hpp
@@ -12,15 +12,16 @@
 #include "../Graphicals/ISound.hpp"
 #include <memory>
 
-class IGame {
+namespace Arcade {
+  class IGame {
     public:
         virtual ~IGame() = default;
 
         //Game
-        virtual void startGame() = 0;
-        virtual void stopGame() = 0;
+        virtual int startGame() = 0;
+        virtual int stopGame() = 0;
         virtual int getScore() = 0;
-        virtual void simulate() = 0;
+        virtual int simulate() = 0;
 
         //Event
         virtual void catchKeyEvent(int key) = 0;
@@ -29,4 +30,5 @@ class IGame {
         virtual std::vector<std::shared_ptr<IEntity>> getEntities() = 0;
         virtual std::vector<std::shared_ptr<IText>> getTexts() = 0;
         virtual std::vector<std::shared_ptr<ISound>> getSounds() = 0;
-};
+    };
+}
